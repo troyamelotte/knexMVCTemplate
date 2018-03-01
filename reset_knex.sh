b0db6f7cf23f254d2cdcb09ec0522faab19d7ff4
troyamelotte <troyamelotte@gmail.com>
2018-03-01 11:06:26 -0700

    template update

---
+++ reset_knex.sh
@@ -0,0 +1,1 @@
+knex migrate:rollback && knex migrate:latest && knex seed:run
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const port = process.env.PORT || 8000;
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.set('view engine', 'ejs');
 
 var routes_setter = require('./config/routes.js');
